6271304248fbe42a645b34a6551e612c39ab6d4e
Olaf Drozdowski <olaf.drozdowski@gmail.com>
2023-11-15 00:06:54 +0100

    Add Wallet Entity

---
+++ app/src/Entity/Wallet.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WalletRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WalletRepository::class)]
+class Wallet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $balance = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $last_updated = null;
+
+
+    /*public function __construct()
+    {
+        $this->balance =  0.0;
+        $this->last_updated = new \DateTime();
+    
+    }*/
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBalance(): ?float
+    {
+        return $this->balance;
+    }
+
+    public function setBalance(float $balance): static
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    public function getLastUpdated(): ?\DateTimeInterface
+    {
+        return $this->last_updated;
+    }
+
+    public function setLastUpdated(\DateTimeInterface $last_updated): static
+    {
+        $this->last_updated = $last_updated;
+
+        return $this;
+    }
+}
