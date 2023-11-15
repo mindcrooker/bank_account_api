2ec62fc378ee46a657b82b331a510cd1142e6de8
Olaf Drozdowski <olaf.drozdowski@gmail.com>
2023-11-15 18:50:12 +0100

    Add additional endpoints to API Controller

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
+### Run project
 To set up project, run: `make start`.
 
-You can than access it locally: `http://localhost:8080/`
+You can then access it locally: `http://localhost:8080/`
 
 
 
@@ -11,5 +12,21 @@ For simplicity the database in `/app/.env` file is set to:
 DATABASE_URL="mysql://root:secret@database:3306/bank_account_api?serverVersion=8.0"
 ```
 
-Available endpoints:
-```/create_wallet``` - POST
+### Access to containers
+
+
+### Available endpoints:
+
+1. ```POST /create_wallet``` - Create new wallet 
+2. ```GET /check_balance/{id}``` - Check balance of the wallet with `{id}`
+3. ```POST /deposit/{id}``` - Deposit funds to wallet with given `{id}`. 
+4. ```POST /withdraw/{id}``` - Withdraw funds from wallet with given `{id}`
+
+To deposit or withdraw funds from wallet using `/deposit/{id}` and `/withdraw/{id}` endpoints, specify the amount in request body, using JSON: 
+```
+{
+    "amount": 10
+}
+``` 
+
+All Routes are available in [ApiController.php](app/src/Controller/ApiController.php)
--- app/src/Controller/ApiController.php
+++ app/src/Controller/ApiController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Wallet;
+use App\Repository\WalletRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
@@ -28,6 +32,86 @@ class ApiController extends AbstractController
         $entityManager->persist($wallet);
 
         $entityManager->flush();
-        return new Response('Saved new wallet with id '.$wallet->getId());
+        return  $this->json('Created new wallet with id '.$wallet->getId());
+    }
+
+    #[Route('/check_balance/{id}', name: 'check_balance', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function checkBalance(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $wallet = $entityManager->getRepository(Wallet::class)->find($id);
+        
+        if (!$wallet) {
+            throw $this->createNotFoundException(
+                'No wallet found for id '. $id
+            );
+        }
+
+        $data = [
+            'id' => $wallet->getId(),
+            'balance' => $wallet->getBalance()
+        ];
+
+        return $this->json($data, Response::HTTP_OK);
+    }
+
+    #[Route('/deposit/{id}', name:'deposit', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function deposit(EntityManagerInterface $entityManager, int $id, Request $request): Response
+    {
+        $wallet = $entityManager->getRepository(Wallet::class)->find($id);
+
+        if (!$wallet) {
+            throw $this->createNotFoundException(
+                'No wallet found for id '. $id
+            );
+        }
+
+        $amount = $request->getPayload()->get("amount");
+        if (!is_numeric($amount) || $amount < 0) {
+            throw new HttpException(
+                400,
+                'Incorrect amount'
+            );
+        }
+
+        $wallet->setBalance($wallet->getBalance() + $amount);
+        $wallet->setLastUpdated(new \DateTime());
+
+        $entityManager->flush();
+
+        return $this->json($amount . ' deposited to wallet with id '.$wallet->getId(), Response::HTTP_OK);
+    }
+
+    #[Route('/withdraw/{id}', name:'withdraw', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function withdraw(EntityManagerInterface $entityManager, int $id, Request $request): Response
+    {
+        $wallet = $entityManager->getRepository(Wallet::class)->find($id);
+
+        if (!$wallet) {
+            throw $this->createNotFoundException(
+                'No wallet found for id '. $id
+            );
+        }
+
+        $amount = ($request->getPayload()->get("amount"));
+        if (!is_numeric($amount) || $amount < 0) {
+            throw new HttpException(
+                400,
+                'Incorrect amount'
+            );
+        }
+
+        if ($wallet->getBalance() < $amount) {
+                throw new HttpException(
+                    400,
+                    'Not enough funds to withdraw'
+                );
+            }
+
+        $wallet->setBalance($wallet->getBalance() - $amount);
+        $wallet->setLastUpdated(new \DateTime());
+
+        $entityManager->flush();
+
+        return $this->json($amount . ' withdrawn from wallet with id '.$wallet->getId(), Response::HTTP_OK);
     }
 }
